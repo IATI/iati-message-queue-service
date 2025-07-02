13e8e2f7bae3afe6aea5085f05c709e208f31bf2
Simon K <6615834+simon-20@users.noreply.github.com>
2025-07-02 20:49:50 +0100

    feat: sample producer *DELETED msgs, custom UUIDs
    This commit adds to the sample producer the ability to send
    *_DELETED messages, and allows the UUID to be specified for
    easier testing. The consumer also now has a perpetual service
    loop mode.

--- sample-python-apps/src/consumer_azsb_library.py
+++ sample-python-apps/src/consumer_azsb_library.py
@@ -1,26 +1,32 @@
-import os
-import asyncio
 import argparse
-from random import randint
+import asyncio
+import os
+
 from azure.servicebus.aio import ServiceBusClient
-from azure.servicebus import ServiceBusMessage
 
 
-async def main(args: argparse.Namespace):
-    conn_str = os.getenv("AZ_SERVICE_BUS_CONNECTION_STRING")
-    topic_name = os.getenv("AZ_SERVICE_BUS_TOPIC_NAME")
-    subscription_name = os.getenv("AZ_SERVICE_BUS_SUBSCRIPTION_NAME")
+async def serviceloop(context: dict):
+    while True:
+        await receive_some_messages(context)
+        await asyncio.sleep(2)
 
-    async with ServiceBusClient.from_connection_string(conn_str=conn_str, logging_enable=True) as servicebus_client:
+
+async def receive_some_messages(context: dict):
+    async with ServiceBusClient.from_connection_string(
+        conn_str=context["connection_string"], logging_enable=True
+    ) as servicebus_client:
         async with servicebus_client:
-            receiver = servicebus_client.get_subscription_receiver(topic_name=topic_name, subscription_name=subscription_name, max_wait_time=1)
+            receiver = servicebus_client.get_subscription_receiver(
+                topic_name=context["topic_name"], subscription_name=context["subscription_name"], max_wait_time=1
+            )
             async with receiver:
-                received_msgs = await receiver.receive_messages(max_wait_time=1, max_message_count=args.num_messages_to_receive)
+                received_msgs = await receiver.receive_messages(
+                    max_wait_time=1800, max_message_count=args.num_messages_to_receive
+                )
                 for msg in received_msgs:
                     print("Message received: ")
-                    print (str(msg))
+                    print(str(msg))
                     await receiver.complete_message(msg)  # complete message, so removed from queue/topic
-
 
 
 if __name__ == "__main__":
@@ -29,6 +35,25 @@ if __name__ == "__main__":
         "--num-messages-to-receive",
         type=int,
         required=True,
-        help="How many messages to receive",
+        help="How many messages to receive (only really relevant if not in service loop)",
     )
-    asyncio.run(main(parser.parse_args()))
+    parser.add_argument(
+        "--run-as-service-loop",
+        action="store_true",
+        required=False,
+        help="Run forever, waiting and receiving messages",
+    )
+
+    args = parser.parse_args()
+
+    context = {
+        "connection_string": os.getenv("AZ_SERVICE_BUS_CONNECTION_STRING"),
+        "topic_name": os.getenv("AZ_SERVICE_BUS_TOPIC_NAME"),
+        "subscription_name": os.getenv("AZ_SERVICE_BUS_SUBSCRIPTION_NAME"),
+        "num_messages_to_receive": args.num_messages_to_receive,
+    }
+
+    if args.run_as_service_loop:
+        asyncio.run(serviceloop(context))
+    else:
+        asyncio.run(receive_some_messages(context))
--- sample-python-apps/src/producer_azsb_library.py
+++ sample-python-apps/src/producer_azsb_library.py
@@ -1,12 +1,13 @@
-import os
-import asyncio
 import argparse
+import asyncio
 import json
+import os
 import uuid
 from datetime import datetime
 from random import randint
-from azure.servicebus.aio import ServiceBusClient
+
 from azure.servicebus import ServiceBusMessage
+from azure.servicebus.aio import ServiceBusClient
 
 
 def get_sample_reporting_org_short_name():
@@ -19,79 +20,116 @@ def get_sample_dataset_short_name(reporting_org: str):
     return "{}-{}".format(reporting_org, names[randint(0, len(names) - 1)])
 
 
-def generate_sample_reporting_org_update_message():
+def generate_sample_delete_message(record_type_to_delete: str, uuid_to_use: str | None = None) -> dict:
+
+    return {
+        "message_type": "{}_DELETED".format(record_type_to_delete.upper()),
+        "message_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        record_type_to_delete: {
+            "id": uuid_to_use if uuid_to_use is not None else str(uuid.uuid4()),
+        },
+    }
+
+
+def generate_sample_reporting_org_update_message(uuid_to_use: str | None = None):
     reporting_org_short_name = get_sample_reporting_org_short_name()
     reporting_org_name = reporting_org_short_name.capitalize() + " Agency"
-    dataset = get_sample_dataset_short_name(reporting_org_short_name)
-    return { "message_type": "REPORTING_ORG_UPDATED",
-             "reporting_org": { 
-                 "id": str(uuid.uuid4()),
-                 "short_name": reporting_org_short_name,
-                 "human_readable_name": reporting_org_name,
-                 "hq_country": "United Kingdom",
-                 "region": "Europe",
-                 "iati_organisation_identifier": "GB-AGY-" + str(randint(100, 900)),
-                 "iati_organisation_type": "National NGO",
-                 "data_portal_url": "https://www.example.org/data-portal/",
-                 "exclusions_policy_url": "https://www.example.org/exclusions.html",
-                 "reporting_source_type": "primary-source" if randint(0, 10) > 5 else "secondary-source",
-                 "default_licence_id": "cc-by",
-                 "contact_email": "sample@example.org",
-                 "address": "123 Main Road, Easytown, Exampleshire",
-                 "phone": "01234 567 890",
-                 "first_publication_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                 "number_of_published_datasets": randint(0, 10),
-             }}
+
+    return {
+        "message_type": "REPORTING_ORG_UPDATED",
+        "message_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "reporting_org": {
+            "id": uuid_to_use if uuid_to_use is not None else str(uuid.uuid4()),
+            "short_name": reporting_org_short_name,
+            "human_readable_name": reporting_org_name,
+            "hq_country": "United Kingdom",
+            "region": "Europe",
+            "iati_organisation_identifier": "GB-AGY-" + str(randint(100, 900)),
+            "iati_organisation_type": "National NGO",
+            "data_portal_url": "https://www.example.org/data-portal/",
+            "exclusions_policy_url": "https://www.example.org/exclusions.html",
+            "reporting_source_type": "primary-source" if randint(0, 10) > 5 else "secondary-source",
+            "default_licence_id": "cc-by",
+            "contact_email": "sample@example.org",
+            "address": "123 Main Road, Easytown, Exampleshire",
+            "phone": "01234 567 890",
+            "first_publication_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "number_of_published_datasets": randint(0, 10),
+        },
+    }
 
 
-def generate_sample_dataset_update_message():
+def generate_sample_dataset_update_message(uuid_to_use: str | None = None):
     reporting_org = get_sample_reporting_org_short_name()
     dataset = get_sample_dataset_short_name(reporting_org)
 
-    return { "message_type": "DATASET_UPDATED",
-             "reporting_org": { 
-                 "id": str(uuid.uuid4()),
-                 "short_name": dataset,
-                 "source_type": "primary-source" if randint(0, 10) > 5 else "secondary-source",
-                 "url": "https://www.example.org/{}.xml".format(dataset),
-                 "last_url_update_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                 "last_metadata_update_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                 "reporting_org_id": str(uuid.uuid4()),
-                 "reporting_org_short_name": reporting_org,
-             }}
+    return {
+        "message_type": "DATASET_UPDATED",
+        "message_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "reporting_org": {
+            "id": uuid_to_use if uuid_to_use is not None else str(uuid.uuid4()),
+            "short_name": dataset,
+            "source_type": "primary-source" if randint(0, 10) > 5 else "secondary-source",
+            "url": "https://www.example.org/{}.xml".format(dataset),
+            "last_url_update_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "last_metadata_update_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "reporting_org_id": str(uuid.uuid4()),
+            "reporting_org_short_name": reporting_org,
+        },
+    }
 
 
-def generate_example_payload(update_message_type):
-    if update_message_type == "dataset":
-        payload = generate_sample_dataset_update_message()
+def generate_example_payload(update_type: str, update_record_type: str, uuid_to_use: str) -> dict:
+    if update_type == "delete":
+        payload = generate_sample_delete_message(update_record_type, uuid_to_use)
     else:
-        payload = generate_sample_reporting_org_update_message()
-    return json.dumps(payload, indent=2)
+        if update_record_type == "dataset":
+            payload = generate_sample_dataset_update_message(uuid_to_use)
+        else:
+            payload = generate_sample_reporting_org_update_message(uuid_to_use)
+    return payload
 
 
-async def send_single_message(update_message_type: str, topic_sender):
-    sample_message = generate_example_payload(update_message_type)
-    message = ServiceBusMessage(sample_message)
+async def send_single_message(update_type: str, update_record_type: str, uuid_to_use: str, topic_sender):
+    msg_payload = generate_example_payload(update_type, update_record_type, uuid_to_use)
+    msg_payload_as_str = json.dumps(msg_payload, indent=2)
+    message = ServiceBusMessage(
+        body=msg_payload_as_str, application_properties={"message_type": msg_payload["message_type"]}
+    )
     await topic_sender.send_messages(message)
-    print("Generated and sent the following sample {} update message:".format(update_message_type))
-    print(sample_message)
+    print("Generated and sent the following sample {} update message:".format(update_type))
+    print(msg_payload_as_str)
 
 
 async def main(args: argparse.Namespace):
-    conn_str = os.getenv("AZ_SERVICE_BUS_CONNECTION_STRING")
-    topic_name = os.getenv("AZ_SERVICE_BUS_TOPIC_NAME")
-    
+    conn_str = os.getenv("AZ_SERVICE_BUS_CONNECTION_STRING", "")
+    topic_name = os.getenv("AZ_SERVICE_BUS_TOPIC_NAME", "")
+
     servicebus_client = ServiceBusClient.from_connection_string(conn_str=conn_str, logging_enable=True)
-    topic_sender = servicebus_client.get_topic_sender(topic_name=topic_name)
-    await send_single_message(args.update_message_type, topic_sender)
+    topic_sender = servicebus_client.get_topic_sender(
+        topic_name=topic_name,
+    )
+    await send_single_message(args.update_type, args.update_record_type, args.uuid_to_use, topic_sender)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="IATI Message Queue Sample App")
     parser.add_argument(
-        "--update-message-type",
-        choices=["dataset", "reporting_org"],
+        "--update-type",
+        choices=["update", "delete"],
         required=True,
         help="Which type of update message to send",
+    )
+    parser.add_argument(
+        "--update-record-type",
+        choices=["dataset", "reporting_org"],
+        required=True,
+        help="Which type of data record to create a message for",
+    )
+    parser.add_argument(
+        "--uuid-to-use",
+        type=str,
+        required=False,
+        help="Use the specified UUID in the message (useful for testing apps against known data)",
     )
     asyncio.run(main(parser.parse_args()))
