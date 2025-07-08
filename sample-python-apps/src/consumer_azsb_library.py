2e2484667fa73f39b44f97f608fc8e78e3b753f3
Simon K <6615834+simon-20@users.noreply.github.com>
2025-07-08 16:04:47 +0100

    feat: sample consumer - exit cleanly

--- sample-python-apps/src/consumer_azsb_library.py
+++ sample-python-apps/src/consumer_azsb_library.py
@@ -1,32 +1,40 @@
 import argparse
 import asyncio
 import os
+from datetime import datetime
 
 from azure.servicebus.aio import ServiceBusClient
 
 
 async def serviceloop(context: dict):
     while True:
-        await receive_some_messages(context)
-        await asyncio.sleep(2)
+        try:
+            print("Sample consumer - service loop - mark - {}".format(datetime.now().isoformat()))
+            await receive_some_messages(context)
+            await asyncio.sleep(2)
+        except asyncio.CancelledError:
+            raise
 
 
 async def receive_some_messages(context: dict):
-    async with ServiceBusClient.from_connection_string(
-        conn_str=context["connection_string"], logging_enable=True
-    ) as servicebus_client:
-        async with servicebus_client:
-            receiver = servicebus_client.get_subscription_receiver(
-                topic_name=context["topic_name"], subscription_name=context["subscription_name"], max_wait_time=1
-            )
-            async with receiver:
-                received_msgs = await receiver.receive_messages(
-                    max_wait_time=1800, max_message_count=args.num_messages_to_receive
+    try:
+        async with ServiceBusClient.from_connection_string(
+            conn_str=context["connection_string"], logging_enable=True
+        ) as servicebus_client:
+            async with servicebus_client:
+                receiver = servicebus_client.get_subscription_receiver(
+                    topic_name=context["topic_name"], subscription_name=context["subscription_name"], max_wait_time=2
                 )
-                for msg in received_msgs:
-                    print("Message received: ")
-                    print(str(msg))
-                    await receiver.complete_message(msg)  # complete message, so removed from queue/topic
+                async with receiver:
+                    received_msgs = await receiver.receive_messages(
+                        max_wait_time=2, max_message_count=args.num_messages_to_receive
+                    )
+                    for msg in received_msgs:
+                        print("Message received: ")
+                        print(str(msg))
+                        await receiver.complete_message(msg)  # complete message, so removed from queue/topic
+    except asyncio.CancelledError:
+        raise
 
 
 if __name__ == "__main__":
@@ -54,6 +62,12 @@ if __name__ == "__main__":
     }
 
     if args.run_as_service_loop:
-        asyncio.run(serviceloop(context))
+        try:
+            asyncio.run(serviceloop(context))
+        except KeyboardInterrupt:
+            print("\nUser pressed Ctrl-C. Stopping receiving messages and exiting")
     else:
-        asyncio.run(receive_some_messages(context))
+        try:
+            asyncio.run(receive_some_messages(context))
+        except KeyboardInterrupt:
+            print("\nUser pressed Ctrl-C. Exiting")
